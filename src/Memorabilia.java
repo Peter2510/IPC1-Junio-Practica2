832b508d93d20ae78af2d7337ef819681cf8a532
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-11 23:57:04 -0600

    agrege metodo para realizar la opcion seleccionada

--- src/Memorabilia.java
+++ src/Memorabilia.java
@@ -33,6 +33,10 @@ public class Memorabilia{
 	int idClientePrestamo;
 	int diasPrestamo;
 
+	//matriz de clientes
+	// TipoDeDato nombre[] = new TipoDeDato[tamaño];
+	//String nombreCliente [] = new String[30];
+
 
 	//constructor de la practica
 
@@ -66,20 +70,83 @@ public class Memorabilia{
    	System.out.println("\nIngresa la opcion a realizar: ");
    	opcion = sc.nextInt();
 
+   	opcionIngresada(opcion);
+
 
 	}
 
    	while (opcion!=9); {
-   		
+
    	// si se ingresa el numero 9, termina el programa
 	System.out.println("\nVuelve pronto ");
    
    	}
 
+   }
+
+   public void opcionIngresada(int opcion){
    	
+   	//Opcion 1 seleccionada
+   	if (opcion==1) {
+
+   		System.out.println("\n1. Prestamo de peliculas ");
+   	}
+
+   	//Opcion 2 seleccionada
+   	if (opcion==2) {
+
+   			System.out.println("\n2. Devolucion de peliculas ");
+   	}
+
+   	//Opcion 3 seleccionada
+   	if (opcion==3) {
+
+   		System.out.println("3. Mostrar datos de las peliculas ");
+   		
+   	}
+
+   	//Opcion 4 seleccionada
+   	if (opcion==4) {
+
+   	System.out.println("4. Registrar nueva pelicula ");
+
+   	}
+
+   	//Opcion 5 seleccionada
+   	if (opcion==5) {
+
+   	System.out.println("5. Ordenar las peliculas en forma ascente respecto al nombre ");
+   	
+
+   	}
+
+   	//Opcion 6 seleccionada
+   	if (opcion==6) {
+
+   	System.out.println("6. Registrar clientes");
+   	
+
+   	}
+
+   	//Opcion 7 seleccionada
+   	if (opcion==7) {
+
+   	System.out.println("7. Mostrar clientes registrados:");
+
+   		
+   	}
+
+   	//Opccion 8 seleccionada
+   	if (opcion==8) {
+
+   	System.out.println("8. Reportes");
+
+   		
+   	}
 
    }
 
 
 
+   
 }
