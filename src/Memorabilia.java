b2c0bccc4f47e8cade069939046d653f8d7f3a30
Pedro Gordillo <pedrogordillo2510@gmail.com>
2021-06-12 01:02:10 -0600

    funcionalidad de registro de clientes funcionando

--- src/Memorabilia.java
+++ src/Memorabilia.java
@@ -12,14 +12,15 @@ public class Memorabilia{
 
 	// definiendo el scanner para toda  la clase
 
-	Scanner sc = new Scanner(System.in);
+	Scanner scanner = new Scanner(System.in);
 	
 	// variables globales  de los clientes
 
 	String nombreCliente;
 	int idCliente;
 	int telefonoCliente;
-	boolean TienePeliculaPrestada;
+	int contadorCliente =0;
+	boolean tienePeliculaPrestada;
 
 	//variables globales de las peliculas
 	String nombrePelicula;
@@ -35,7 +36,15 @@ public class Memorabilia{
 
 	//matriz de clientes
 	// TipoDeDato nombre[] = new TipoDeDato[tamaño];
-	//String nombreCliente [] = new String[30];
+	//matriz de nombres
+	String matrizNombreCliente[] = new String[30];
+
+	// Matriz de id
+	int matrizIdCliente[] = new int[30];
+
+	// Matriz de telefono cliente
+	int matrizTelefonoCliente[] = new int[30];
+
 
 
 	//constructor de la practica
@@ -64,11 +73,11 @@ public class Memorabilia{
    	System.out.println("4. Registrar nueva pelicula ");
    	System.out.println("5. Ordenar las peliculas en forma ascente respecto al nombre ");
    	System.out.println("6. Registrar nuevos clientes");
-   	System.out.println("7. Mostrar clientes registrados:");
+   	System.out.println("7. Mostrar clientes registrados");
    	System.out.println("8. Reportes");
    	System.out.println("9. Salir");
-   	System.out.println("\nIngresa la opcion a realizar: ");
-   	opcion = sc.nextInt();
+   	System.out.print("\nIngresa la opcion a realizar: ");
+   	opcion = scanner.nextInt();
 
    	opcionIngresada(opcion);
 
@@ -87,64 +96,113 @@ public class Memorabilia{
    public void opcionIngresada(int opcion){
    	
    	//Opcion 1 seleccionada
-   	if (opcion==1) {
+   		if (opcion==1) {
 
    		System.out.println("\n1. Prestamo de peliculas ");
-   	}
+   		}
 
    	//Opcion 2 seleccionada
-   	if (opcion==2) {
+   		if (opcion==2) {
 
    			System.out.println("\n2. Devolucion de peliculas ");
-   	}
+   		}
 
    	//Opcion 3 seleccionada
-   	if (opcion==3) {
+   		if (opcion==3) {
 
    		System.out.println("3. Mostrar datos de las peliculas ");
    		
-   	}
+   		}
 
-   	//Opcion 4 seleccionada
-   	if (opcion==4) {
+   		//Opcion 4 seleccionada
+   		if (opcion==4) {
 
-   	System.out.println("4. Registrar nueva pelicula ");
+   		System.out.println("4. Registrar nueva pelicula ");
 
-   	}
+   		}
 
-   	//Opcion 5 seleccionada
-   	if (opcion==5) {
+   		//Opcion 5 seleccionada
+   		if (opcion==5) {
 
-   	System.out.println("5. Ordenar las peliculas en forma ascente respecto al nombre ");
+   		System.out.println("5. Ordenar las peliculas en forma ascente respecto al nombre ");
+   	
+   		}
+
+   		//Opcion 6 seleccionada
+   		if (opcion==6) {
+
+   		registrarClientes();
    	
 
-   	}
+   		}
 
-   	//Opcion 6 seleccionada
-   	if (opcion==6) {
+   		//Opcion 7 seleccionada
+   		if (opcion==7) {
 
-   	System.out.println("6. Registrar clientes");
-   	
+   			if (contadorCliente==0) {
+   				System.out.print("\nAun no hay clientes registrados\n");
+   			}
 
-   	}
+   			else{
 
-   	//Opcion 7 seleccionada
-   	if (opcion==7) {
+   				String prestamo;
 
-   	System.out.println("7. Mostrar clientes registrados:");
+   			for (int i =0 ; i < contadorCliente ; i++ ) {
 
-   		
-   	}
+   				if (tienePeliculaPrestada==false) {
+   					 prestamo = "no";
+   				}
 
-   	//Opccion 8 seleccionada
-   	if (opcion==8) {
+   				else{
+   					 prestamo = "si";
+   				}
+   				System.out.println("\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+   				System.out.println("\nid: " + matrizIdCliente[i]+", Nombre del Cliente: "+ matrizNombreCliente[i] + ", telefono: " + matrizTelefonoCliente[i] + ", Posee peliculas de la tienda: "+ prestamo );
+   			}
 
-   	System.out.println("8. Reportes");
+   		System.out.println();
 
-   		
-   	}
+   		}
+
+   			}
+   			
+
+   		//Opccion 8 seleccionada
+   		if (opcion==8) {
+
+   		System.out.println("8. Reportes");
+
+   		}
 
    }
+
+   // Registrar cliente nuevo
+   public void registrarClientes(){
+
+   		System.out.println("\n-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+   		System.out.println("\nBienvenido al registro de clientes");
+
+   		//Ingresar nonmbre del cliente
+      	System.out.print("\nIngrese el nombre del cliente: ");
+        nombreCliente = scanner.nextLine();
+        nombreCliente = scanner.nextLine();
+		matrizNombreCliente[contadorCliente]=nombreCliente;
+		
+		//Ingresar el id del cliente
+		System.out.print("\nIngrese el id del cliente: ");
+        idCliente = scanner.nextInt();
+		matrizIdCliente[contadorCliente]=idCliente;
+
+		//Ingresar el telefono del cliente
+		System.out.print("\nIngrese el telefono del cliente: ");
+        telefonoCliente = scanner.nextInt();
+		matrizTelefonoCliente[contadorCliente]=telefonoCliente;
+
+		//Pelicula prestada
+		tienePeliculaPrestada = false;
+		contadorCliente++;
+
+      }
 
 
 
